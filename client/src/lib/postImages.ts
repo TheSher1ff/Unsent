fecee6c4663a08d57241d0cbc40a91b7d4c1ab4c
The Sheriff <sheriffalli.t@gmail.com>
2026-06-27 01:57:21 -0400

    Add postImages array with image paths

---
+++ client/src/lib/postImages.ts
@@ -0,0 +1,12 @@
+export const postImages = [
+  "/images/posts/1.jpg",
+  "/images/posts/2.jpg",
+  "/images/posts/3.jpg",
+  "/images/posts/4.jpg",
+  "/images/posts/5.jpg",
+  "/images/posts/6.jpg",
+  "/images/posts/7.jpg",
+  "/images/posts/8.jpg",
+  "/images/posts/9.jpg",
+  "/images/posts/10.jpg",
+];
